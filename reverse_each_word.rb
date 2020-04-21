1c84b02a1533e12a75538441c704481d6e6c4f5d
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-21 22:18:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   converted_to_arr = string.split(" ")
   reversed_arr = []
   converted_to_arr.each do |string|
-    reversed_arr.push(string)
+    reversed_arr.push(string.reverse)
   end
 
 end
