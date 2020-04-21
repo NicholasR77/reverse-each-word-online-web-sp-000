3cda36f00caef73b0a1ed0722772afbad7082401
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-21 22:16:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,8 @@
 def reverse_each_word(string)
   converted_to_arr = string.split(" ")
-  puts converted_to_arr
+  converted_to_arr.each do |string|
+    puts string.reverse
+  end  
 end
 
 reverse_each_word("Hello there, and how are you?")
